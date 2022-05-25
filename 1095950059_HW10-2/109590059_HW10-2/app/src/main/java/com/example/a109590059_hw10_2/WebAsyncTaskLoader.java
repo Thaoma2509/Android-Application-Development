c5521c0097ea36ece23f035b11ed489ebe815c8f
Thaoma2509 <100550724+Thaoma2509@users.noreply.github.com>
2022-05-25 20:53:27 +0800

    Update on 2022/05/25

---
+++ 1095950059_HW10-2/109590059_HW10-2/app/src/main/java/com/example/a109590059_hw10_2/WebAsyncTaskLoader.java
@@ -0,0 +1,32 @@
+package com.example.a109590059_hw10_2;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class WebAsyncTaskLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    public WebAsyncTaskLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        try {
+            return GetWebData.getHtml(mQueryString);
+        } catch (Exception e) {
+            return String.format("error %s", e);
+        }
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+}
